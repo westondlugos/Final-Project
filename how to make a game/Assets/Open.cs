e0dc1b0cbc1581638745213b9f1a84a593c48f79
ShroffS0649 <ShroffS0649@sduhsd.lan>
2017-05-10 10:06:30 -0700

    v.1

---
+++ how to make a game/Assets/Open.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Open : MonoBehaviour {
+	
+	public Rigidbody rb;
+
+	public float open = 0f;
+
+	void FixedUpdate () 
+	{
+		rb.AddForce (open, 0, 0 * Time.deltaTime);
+
+		if (Input.GetKey("f"))
+		{
+			rb.AddForce (25 * Time.deltaTime, 0, 0);
+		}
+	}
+}
