3d0661d3a250b124208da0f6853b00ae96011b59
ShroffS0649 <ShroffS0649@sduhsd.lan>
2017-05-16 10:24:13 -0700

    Scene2 Chest

---
+++ how to make a game/Assets/Chest.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chest : MonoBehaviour {
+	public enum State{
+		open,
+		closed,
+		between,
+	}
+
+	public State state;
+
+	void Start () {
+		state = Chest.State.closed;
+	}
+
+
+	void Update (){
+		
+	}
+
+
+	public void OnMouseEnter(){
+		Debug.Log ("Enter");
+	}
+
+
+	public void OnMouseExit(){
+		Debug.Log("Exit");
+	}
+
+
+	public void OnMouseUp(){
+		Debug.Log("Up");
+		if (State == Chest.State.closed)
+			Open ();
+		else
+			Close ();
+
+	}
+	private void Open(){
+	Animation.Play("Open");
+	state = Chest.State.open;
+	
+}
+	private void Close(){
+	Animation.Play("Close");
+	state = Chest.State.closed;
+	
+}
+
+}
